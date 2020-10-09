a8931de2dfc10b87f8189509faf06c2015a63505
nhulox97 <sergiobernal909@gmail.com>
2020-10-08 21:46:03 -0600

    tarea1.sh done

--- tarea1.sh
+++ tarea1.sh
@@ -35,9 +35,8 @@ function file_exists() {
 }
 
 function create_dir() {
-    local dir=''
     local is_valid=0
-    echo -e "\n################# Crear mi directorio ###################\n"
+    echo -e "################# Crear mi directorio ###################\n"
     while [ $is_valid -ne 1 ]; do
         read -p '-> Ingrese el nombre del directorio: ' dir
         if [[ $dir =~ ^[a-zA-Z0-9_.-]*$ ]]; then
@@ -56,7 +55,7 @@ function create_dir() {
 
 function create_file() {
     local is_valid=0
-    echo -e "\n################# Crear mi archivo ###################\n"
+    echo -e "################# Crear mi archivo ###################\n"
     while [ $is_valid -ne 1 ]; do
         read -p '-> Ingrese el nombre del archivo: ' file
         if [[ $file =~ ^[a-zA-Z0-9_.-]*$ ]]; then
@@ -110,57 +109,134 @@ function set_permissions(){
     esac
 }
 
-# validar mediante input del usuario si es archivo o directorio
 function give_permissions() {
-    echo -e "\n################# Brindar permisos ###################\n"
+    echo -e "################# Brindar permisos ###################\n"
     option=0
     while [ $option -ne 8 ]; do
         read -p '-> Ingrese el nombre del archivo o directorio al cual se brindarán los permisos: ' name
-        if [[ $file =~ ^[a-zA-Z0-9_.-]*$ ]]; then
-            read -p '-> Es un archivo o directorio? (a)rchivo (d)irectorio: ' search_option
-            if [ $search_option = 'a' ]; then 
-                if [ -f $name ]; then
-                    permissions_menu
+        if [[ $file =~ ^[\\/a-zA-Z0-9_.-]*$ ]]; then
+            if [ -f $name ]; then
+                permissions_menu
+                read -p "-> Ingrese el tipo de permisos a asignar al archivo: " option
+                if [ $option -ne 8 ]; then
+                    echo -e "Permisos del archivo antes de cambiarlos"
+                    echo `ls -l $name`
+                    set_permissions "$option" "$name"
+                    echo -e "Permisos del archivo despues de cambiarlos"
+                    echo `ls -l $name`
+                    option=8
+                fi 
+            elif [ -a $name ]; then
+                permissions_menu
+                if [ $option -ne 8 ]; then
                     read -p "-> Ingrese el tipo de permisos a asignar al archivo: " option
-                    if [ $option -ne 8 ]; then
-                        echo -e "Permisos del archivo antes de cambiarlos"
-                        echo `ls -l $name`
-                        set_permissions "$option" "$name"
-                        echo -e "Permisos del archivo despues de cambiarlos"
-                        echo `ls -l $name`
-                        option=8
-                    fi 
-                else
-                    echo -e "\nEl Archivo no existe, si el archivo esta en otro directorio verificar si la ruta es correcta\n"
-                fi
-            elif [ $search_option = 'd' ]; then 
-                if [ -a $name ]; then
-                    permissions_menu
-                    if [ $option -ne 8 ]; then
-                    read -p "-> Ingrese el tipo de permisos a asignar al archivo: " option
-                        echo -e "Permisos del archivo antes de cambiarlos"
-                        echo `ls -ld $name`
-                        set_permissions "$option" "$name"
-                        echo -e "Permisos del archivo despues de cambiarlos"
-                        echo `ls -ld $name`
-                        option=8
-                    fi 
-                else
-                    echo -e "\nEl directorio no existe, si el directorio esta en otro directorio verificar si la ruta es correcta\n"
-                fi
+                    echo -e "Permisos del archivo antes de cambiarlos"
+                    echo `ls -ld $name`
+                    set_permissions "$option" "$name"
+                    echo -e "Permisos del archivo despues de cambiarlos"
+                    echo `ls -ld $name`
+                    option=8
+                fi 
+            else
+                echo -e "\nEl directorio o archivo no existe, si el directorio o archivo esta en otro directorio verificar si la ruta es la correcta\n"
             fi
+
         else
-            echo - "\nIngresar un nombre de archivo o directorio valido\n"
+            echo -e "\nIngresar un nombre de archivo o directorio valido\n"
         fi
     done
 }
 
 function list_dirs_and_files_by_route() {
-    echo 'Funcion: list_dirs_and_files_by_route'
+    local is_valid=0
+    echo -e "################# Listar mi directorio ###################\n"
+    while [ $is_valid -ne 1 ]; do
+        read -p '-> Ingrese el nombre del directorio: ' dir
+        if [[ $dir =~ ^[\\/a-zA-Z0-9_.-]*$ ]]; then
+            check_dir=$(dir_exists $dir)
+            if [ $check_dir = 'y' ]; then
+                # dir_listed=`ls -lh --block-size=MB $dir`
+                echo '###############################################'
+                echo -e "Lista de archivos y directorios del directorio: $dir\n"
+                # echo $dir_listed
+                ls -lh --block-size=MB $dir
+                echo -e "\n"
+                is_valid=1
+            elif [ $check_dir = 'n' ]; then
+                echo -e "\nEl directorio $dir no existe, favor verificar la ruta"
+            fi
+        else
+            echo -e "\nIngresar un nombre de archivo o directorio valido\n"
+        fi
+    done
+}
+
+function compression_menu() {
+    echo -e "\n========Seleccione el tipo de compresión"
+    echo '(1). zip'
+    echo '(2). tar'
+}
+
+function do_compress() {
+    echo -e "\n"
+    read -p "-> Ingrese el nombre que le asignará al comprimido: " filename
+    if [ $1 -eq 1 ]; then
+        # zip compression
+        compress=`zip -r $3/$filename.zip $2`
+        echo -e "\nEl directorio $2 se comprimió en $3/$filename.zip"
+    elif [ $1 -eq 2 ]; then
+        # tar compression
+        compress=`tar -cvf $3/$filename.tar $2`
+        echo -e "\nEl directorio $2 se comprimió en $3/$filename.zip"
+    fi
 }
 
 function compress_files() {
-    echo 'Funcion: compress_files'
+    local is_valid=0
+    echo -e "################# Comprimir mi directorio ###################\n"
+    while [ $is_valid -ne 1 ]; do
+        # Solicitando directorio origen
+        read -p '-> Ingrese el nombre del directorio a comprimir: ' org_dir
+        # validando rura
+        if [[ $org_dir =~ ^[\\/a-zA-Z0-9_.-]*$ ]]; then
+            check_dir=$(dir_exists $org_dir)
+            if [ $check_dir = 'y' ]; then
+                # Solicitando directorio destino
+                echo -e "\n"
+                read -p "-> Ingrese la ruta en donde se almacenará el comprimido, si desea guardar el comprimido en la misma carpeta escriba 'origen': " dest_dir
+                # comprimir en carpeta origen
+                if [ $dest_dir = 'origen' ]; then
+                    compression_menu    
+                    read -p '-> Ingrese su opción: ' option
+                    echo $option
+                    result="$(do_compress $option $org_dir $org_dir)"
+                    clear
+                    echo $result
+                    is_valid=1
+                    # comprimir en otra ruta 
+                elif [[ $dest_dir =~ ^[\\/a-zA-Z0-9_.-]*$ ]]; then
+                    check_dir=$(dir_exists $dest_dir)
+                    if [ $check_dir = 'y' ]; then
+                        compression_menu    
+                        read -p '-> Ingrese su opción: ' option
+                        echo $option
+                        result="$(do_compress $option $org_dir $dest_dir)"
+                        clear
+                        echo $result
+                        is_valid=1
+                    elif [ $check_dir = 'n' ]; then
+                        echo -e "\nEl directorio destino $dest_dir no existe, favor verificar la ruta"
+                    fi
+                else
+                    echo -e "\nIngresar un nombre de archivo o directorio valido\n"
+                fi
+            elif [ $check_dir = 'n' ]; then
+                echo -e "\nEl directorio origen $org_dir no existe, favor verificar la ruta"
+            fi
+        else
+            echo -e "\nIngresar un nombre de archivo o directorio valido\n"
+        fi
+    done
 }
 
 # Testing zone
@@ -174,21 +250,27 @@ while [ $menu_condition != 'n' ]; do
     read -p '-> Ingrese su opcion: ' option
     case $option in
         1)
+            clear
             create_dir;;
         2)
+            clear
             create_file;;
         3)
+            clear
             give_permissions;;
         4)
+            clear
             list_dirs_and_files_by_route;;
         5)
+            clear
             compress_files;;
         6)
+            clear
             menu_condition='n';;
     esac
     if [ $option -ne 6 ]; then
-        echo -e "\n"
         read -p '-> Desea realizar otra operacion? (s)i (n)o: ' menu_condition
         echo -e "\n"
     fi
+    clear
 done
